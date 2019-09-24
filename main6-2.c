1e8727ea638d7a5cbe6321d900defee6ce421df7
Simsoobin <55381009+Simsoobin@users.noreply.github.com>
2019-09-24 16:07:18 +0900

    04-2_op

--- main6-2.c
+++ main6-2.c
@@ -4,7 +4,7 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int x =3;
+    int x =3;
     int cond1, cond2;
     
     cond1 = 2<x && x<5;
